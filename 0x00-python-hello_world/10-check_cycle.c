300f792f5ac6e5dfcef1e525a1775e1f4e50d456
Udeme100 <udemx200@gmail.com>
2023-01-10 19:22:53 +0000

    repeat this hard task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,10 +16,10 @@ int check_cycle(listint_t *list)
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
-		fast = fast->next->next->;
+		fast = fast->next->next;
 		if (slow == fast)
 			return (1);
 	}
 
-		return (0);
+	return (0);
 }
